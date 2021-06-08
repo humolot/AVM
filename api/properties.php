f6bcb68018a6f5e47ec0cb737bad19da42341487
madcowbr@gmail.com <madcowbr@gmail.com>
2021-06-08 15:20:14 +0000

    File generated by Skaffolder - user: madcowbr@gmail.com

---
+++ api/properties.php
@@ -0,0 +1,10 @@
+<?php 
+	$db_AVM_db_Url = $_ENV["DB_HOST"] ?? '127.0.0.1';
+	$db_AVM_db_User = 'root';
+	$db_AVM_db_Pass = '';
+	$db_AVM_db_DbName = 'avm_db';
+	
+	
+	$jwt_secret_key = 'mySecretKey';
+	
+?>
